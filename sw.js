db2fb3ed38052cccf12bbc3f36a7bf336553a8e9
Tim McMaster <tim@mcmaster.id.au>
2019-05-15 08:12:32 +1000

    test deploy

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-importScripts("/precache-manifest.6abf0ce42ad76f77d81d57068fd127e3.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.097f115d765af9136799a50cbe0e4c82.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 
