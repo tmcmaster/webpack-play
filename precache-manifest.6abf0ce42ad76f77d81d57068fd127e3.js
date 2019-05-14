775c5daf3a224ce2e4ecd2e4d9ba9835b94d512c
Tim McMaster <tim@mcmaster.id.au>
2019-05-15 07:59:10 +1000

    init

---
+++ precache-manifest.6abf0ce42ad76f77d81d57068fd127e3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e14857164f0d5b4dba3",
+    "url": "/main.1e148571.js"
+  },
+  {
+    "revision": "a105ea81c6fbd5932caf51232d7cccee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1fd2666c41624563d5f54b012c56acd",
+    "url": "/vendor/custom-elements-es5-adapter.js"
+  },
+  {
+    "revision": "e984e03e40948cb775cf97fce7bf6532",
+    "url": "/styles.css"
+  },
+  {
+    "revision": "c7f3db9007916717a84368850808bdd2",
+    "url": "/manifest.webmanifest"
+  },
+  {
+    "revision": "e044a63e034bf10304dad73138b8c74b",
+    "url": "/vendor/webcomponents-loader.js"
+  },
+  {
+    "revision": "039cece33b43e3ae0215cdcd2ade8c6b",
+    "url": "/vendor/bundles/webcomponents-ce.js"
+  },
+  {
+    "revision": "104b3fda1e992ee77cf88e2ba8542a4a",
+    "url": "/img/icons/icon-144x144.png"
+  },
+  {
+    "revision": "9b5e91ababe48f96596f6b3494d2fa3e",
+    "url": "/img/icons/favicon.png"
+  },
+  {
+    "revision": "91a9501d74924f5da51f71dceffe6db5",
+    "url": "/img/icons/icon-192x192.png"
+  },
+  {
+    "revision": "23fc57b3e5ca7fcce96bb3d8de543819",
+    "url": "/img/icons/icon-512x512.png"
+  },
+  {
+    "revision": "0f94d1d1fa6eb56e54ceba65a10d5cab",
+    "url": "/vendor/bundles/webcomponents-sd.js"
+  },
+  {
+    "revision": "ed70043d6ffd397cd8a5785a91deedcc",
+    "url": "/vendor/bundles/webcomponents-sd-ce.js"
+  },
+  {
+    "revision": "ea4c0648b79eb102853d55e80d75daf4",
+    "url": "/vendor/webcomponents-bundle.js"
+  },
+  {
+    "revision": "c73899f69a673e93653c018e4a2be04b",
+    "url": "/vendor/bundles/webcomponents-sd-ce-pf.js"
+  }
+];
